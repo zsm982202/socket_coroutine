cc156dc7ba5a5959e6139fee71c142f183ba12ff
zhangqi <zhangqi_gsts@foxmail.com>
2022-04-01 01:26:53 +0800

    初步功能完成

--- xfiber.h
+++ xfiber.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <map>
+#include <list>
 #include <queue>
 #include <vector>
 #include <string>
 #include <functional>
 #include <ucontext.h>
+
+#include "log.h"
 
 typedef enum {
     INIT = 0,
@@ -21,22 +24,51 @@ public:
 
     ~XFiber();
 
-    void AddReadyTask(Fiber *fiber);
-    
+    void WakeupFiber(Fiber *fiber);
+
     void AddTask(std::function<void()> run, size_t stack_size = 0, std::string fiber_name="");
 
     void Dispatch();
 
     void Yield();
 
+    void SwitchToSchedFiber();
+
+    bool RegisterFdToSchedWithFiber(int fd, Fiber *fiber, int op);
+
+    bool UnregisterFdFromSched(int fd);
+
     ucontext_t *SchedCtx();
 
+    static XFiber *xfiber() {
+        static thread_local XFiber xf;
+        return &xf;
+    }
+
+    inline Fiber *CurrFiber() {
+        return curr_fiber_;
+    }
+
 private:
+    int efd_;
+    
     std::deque<Fiber *> ready_fibers_;
 
     ucontext_t sched_ctx_;
 
     Fiber *curr_fiber_;
+
+    struct WaitingFibers {
+        Fiber *r_, *w_;
+        WaitingFibers() {
+            r_ = nullptr;
+            w_ = nullptr;
+        }
+    };
+
+    std::map<int, WaitingFibers> io_waiting_fibers_;
+    // 会不会出现一个fd的读/写被多个协程监听？？不会！
+    // 但是一个fiber可能会监听多个fd，实际也不存在，一个连接由一个协程处理
 };
 
 
@@ -59,9 +91,12 @@ public:
 
     void Yield();
 
+    uint64_t Seq();
+
     static void Start(Fiber *fiber);
 
 private:
+    uint64_t seq_;
     XFiber *xfiber_;
     std::string fiber_name_;
     FiberStatus status_;
