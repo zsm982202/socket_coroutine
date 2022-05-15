93472c75b817eb2115761f311281126f7526302e
思民 展 <zsm982202@Outlook.com>
2022-05-15 21:44:26 +0800

    20220515 17:34

--- main.cpp
+++ main.cpp
@@ -22,14 +22,14 @@ int main() {
     schedule->CreateCoroutine([&] {
         Server server = Server::ListenTCP(8888);
         while(true) {
-            Connection* conn = server.Accept();
+            Connection* conn = server.Accept(); //如果Accept不到客户端立刻放弃cpu，单线程是阻塞在这里，多线程会浪费cpu在这，直到Accept到客户端
             schedule->CreateCoroutine([conn] {
                 while(true) {
                     char recv_buf[512];
-                    int n = conn->Read(recv_buf, 512, 2000);
+                    int n = conn->Read(recv_buf, 512, 2000); //如果读完立刻放弃cpu
                     if(n <= 0)
                         break;
-                    if(conn->Write("+Recv\r\n", 7, 2000) <= 0)
+                    if(conn->Write("+Recv\r\n", 7, 2000) <= 0) //如果写完立刻放弃cpu
                         break;
                 }
             }, 0, "server");
