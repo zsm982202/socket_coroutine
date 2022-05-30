d5f7fd8c9562fa447f4ae3201bdc65c7d8da087b
zsm982202 <zsm982202@outlook.com>
2022-05-29 23:41:07 -0700

    20220530

--- main.cpp
+++ main.cpp
@@ -25,12 +25,14 @@ int main() {
             Connection* conn = server.Accept();
             schedule->CreateCoroutine([conn] {
                 while(true) {
-                    char recv_buf[512];
+                    char recv_buf[512], write_buf[512] = "+";
                     int n = conn->Read(recv_buf, 512, 2000);
                     if(n <= 0)
                         break;
-                    if(conn->Write("+Recv\r\n", 7, 2000) <= 0)
-                        break;
+					strcat(write_buf, recv_buf);
+                    //if(conn->Write("+Recv\r\n", 7, 2000) <= 0)
+                    if(conn->Write(write_buf, n + 1, 2000) <= 0)
+						break;
                 }
             }, 0, "server");
         }
